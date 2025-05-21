91bdedf926f8148759a5b4408815878446352c28
KAY PEE <kelzangpenjor1104@gmail.com>
2025-05-21 20:26:34 +0530

    Fourth commit

--- pages/api/jobs/recruiter.js
+++ pages/api/jobs/recruiter.js
@@ -1,5 +1,5 @@
 import { getSession } from 'next-auth/react';
-import dbConnect from '../../../lib/dbConnect';
+import dbConnect from '../../../lib/mongodb';
 import Job from '../../../models/Job';
 
 export default async function handler(req, res) {
